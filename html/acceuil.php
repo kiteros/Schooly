b3d9e4d86793a5f1a0ee64a6d55c7f76cd51577b
Kiteros <jules.eschbach@icloud.com>
2016-03-23 19:22:23 +0100

    Amélioration de la position Css des éléments de la pae d'acceuil

--- html/acceuil.php
+++ html/acceuil.php
@@ -11,14 +11,26 @@
     </head>
 
 	<body>
+		<div class="navigateur">
+			<h1> Schooly </h1>
+		</div>
 		<div class="partIndex1">
-			<div class="presentationIndex">
-				<h2> Schooly, le nec plus ultra des études collectives </h2>
-				<h3> Schooly est un site gratuit et facile d'accès permettant de gérer la vie d'une classe </h3>
+			<div class="coupleIndent">
+				<div class="presentationIndex">
 
-			</div>
-			<div>
-				<h2> Connexion ou inscrpition </h2>
+					<h2> Schooly, le nec plus ultra des études collectives </h2>
+					<h3> Schooly est un site gratuit et facile d'accès permettant de gérer la vie d'une classe </h3>
+
+				</div>
+				<div class="connect">
+					<br/>
+					<div class="strike">
+						<form action="#" method="post">
+							<input type = "text" placeholder="Nom d'utilisateur ou adresse email" /><br/><br/>
+							<input type = "password" placeholder="Mot de passe" />
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
