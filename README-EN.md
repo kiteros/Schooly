79ed78c43ec1e54d71203f16ca9f2c2602c1239a
Kiteros <jules.eschbach@icloud.com>
2016-03-23 18:04:36 +0100

    Ajout d'un fond d'ecran et d'une redirection de l'index

--- README-EN.md
+++ README-EN.md
@@ -1,3 +1,3 @@
 # Schooly
-A new way to see school. Network for student
+A new way to see school. This is a network for students.
 This social network helps student to share files using public spaces. You can chat with your classMates and help them to do their homeworks. You can also organize events or participate at events organized by your school.
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-
-<html>
-
-    <head>
-
-        <meta charset="utf-8" />
-		<link rel="stylesheet" href="style/styleAcceuil.css" />
-        <title>Schooly</title>
-
-    </head>
-
-	<body>
-		<div class="presentationIndex">
-			<h2> Schooly, le nec plus ultra des études collectives </h2>
-			<h3> Schooly est un site gratuit et facile d'accès permettant de gérer la vie d'une classe </h3>
-
-		</div>
-		<div>
-			<h2> Connexion ou inscrpition </h2>
-		</div>
-		
-	</body>
-
-</html>
-
-
+<?php
+	header('Location: html/acceuil.php');
+	exit;
+?>
