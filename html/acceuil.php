9fb39fac9e7991d774d7af2cc3474e206b4fa9a1
Kiteros <jules.eschbach@icloud.com>
2016-03-24 19:27:06 +0100

    Finition du design de la page d'acceuil

--- html/acceuil.php
+++ html/acceuil.php
@@ -12,25 +12,37 @@
 
 	<body>
 		<div class="navigateur">
-			<h1> Schooly </h1>
+			<div class = "naviIndex">
+				<nav>
+					<ul>
+						<li id="logo"><h2>LOGO</h2></li>
+			            <li><a href="principal.php">Flux</a></li>
+			            <li><a href="account.php">Mon compte</a></li>
+			            <li><a href="stats.php">Stats</a></li>
+			            <li><a href="addMark.php">Ajouter</a></li>
+			            <li class = "buttonConnectIndexBar">
+			            	<form action="#" method = "post">
+			            		<input type="submit" value="Inscription" class="bCoPetit" />
+			            	</form>
+			            </li>
+			        </ul>
+				</nav>
+			</div>
 		</div>
 		<div class="partIndex1">
-			<div class="coupleIndent">
-				<div class="presentationIndex">
-
-					<h2> Schooly, le nec plus ultra des études collectives </h2>
-					<h3> Schooly est un site gratuit et facile d'accès permettant de gérer la vie d'une classe </h3>
-
-				</div>
+			<div class="couple">
 				<div class="connect">
-					<br/>
 					<div class="strike">
 						<form action="#" method="post">
 							<input type = "text" placeholder="Nom d'utilisateur ou adresse email" /><br/><br/>
-							<input type = "password" placeholder="Mot de passe" />
+							<input type = "password" placeholder="Mot de passe" /><br/><br/>
+							<input type = "submit" value = "Connexion" class = "bC" />
 						</form>
 					</div>
+					<br/><br/>
 				</div>
+				<h1> Schooly, le nec plus ultra des études collectives </h1>
+				<h2> Schooly est un site gratuit et facile d'accès permettant de gérer la vie d'une classe </h2>
 			</div>
 		</div>
 	</body>
